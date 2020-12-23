c90c07945eb1e91d354b78b802567ccc55d93fb8
theresageorge1997 <theresageorgr1997@gmail.com>
2020-12-23 09:10:24 +0530

    e-commerce using spring boot

---
+++ STS_Workspace/e-commerce_App_THERESA_GEORGE/src/main/java/com/ust/ecommerce/ECommerceAppTheresaGeorgeApplication.java
@@ -0,0 +1,13 @@
+package com.ust.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceAppTheresaGeorgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceAppTheresaGeorgeApplication.class, args);
+	}
+
+}
---
+++ STS_Workspace/e-commerce_App_THERESA_GEORGE/src/test/java/com/ust/ecommerce/ECommerceAppTheresaGeorgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ust.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceAppTheresaGeorgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
